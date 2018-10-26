c67954792bc0184aa1e23fcd3fb639f2a754a0b7
Stefan <karl_stefan@yahoo.se>
2018-10-26 12:21:51 +0200

    Update PS51lathund.ps1
    version 0.1b

--- PS51lathund.ps1
+++ PS51lathund.ps1
@@ -1,27 +1,24 @@
 <#
-PS51lathund.ps1 
+Lathund fÃ¶r PowerShell 5.1 noviser version 0.1
+Â© 2018 av Stefan Blecko
 
-Lathund för PowerShell 5.1 noviser version 0.1
-© 2018 av Stefan Blecko
-
-Kopiera gärna innehållet i filen till C:\Users\[ditt användarnamn]\Documents\
-WindowsPowerShell\Microsoft.PowerShell_profile.ps1 och testa och se vad som händer.
+Kopiera gÃ¤rna innehÃ¥llet i filen till C:\Users\[ditt anvÃ¤ndarnamn]\Documents\
+WindowsPowerShell\Microsoft.PowerShell_profile.ps1 och testa och se vad som hÃ¤nder.
 #>
 
 $PSprojfoldername = "PSproj"
 Set-Variable $HOME C:\Users\$env:USERNAME
 
-Write-Host "Användare: $env:USERNAME | Projektmapp: $PSprojfoldername`n" -BackgroundColor DarkBlue
+Write-Host "AnvÃ¤ndare: $env:USERNAME | Projektmapp: $PSprojfoldername`n" -BackgroundColor DarkBlue
 
 if (Test-Path -Path "C:\Users\$env:USERNAME\Desktop\$PSprojfoldername") {
 }
 else {
     New-Item -Path "C:\Users\$env:USERNAME\Desktop\" -Name $PSprojfoldername -ItemType "directory" | Out-Null
 }
-New-PSDrive -Name $PSprojfoldername -PSProvider FileSystem -Root "C:\Users\$env:USERNAME\Desktop\$PSprojfoldername" -Description "Välkommen till skript lådan." | Out-Null
+New-PSDrive -Name $PSprojfoldername -PSProvider FileSystem -Root "C:\Users\$env:USERNAME\Desktop\$PSprojfoldername" -Description "VÃ¤lkommen till skript lÃ¥dan." | Out-Null
 Set-Location ($PSprojfoldername + ":")
-Write-Host "PÅGÅENDE POWERSHELL PROJEKT`n" -ForegroundColor White -BackgroundColor Black
-Get-ChildItem -Name *.ps1
-Write-Host " " 
-Write-Host "KONSOLFÖNSTRETS KORTKOMMANDON" -ForegroundColor White -BackgroundColor Black
+Write-Host "PÃ…GÃ…ENDE POWERSHELL PROJEKT" -ForegroundColor White -BackgroundColor Black
+Get-ChildItem *.ps1 | Select-Object Name, LastWriteTime | Sort-Object LastWriteTime -Descending | Out-Host
+Write-Host "KONSOLFÃ–NSTRETS KORTKOMMANDON" -ForegroundColor White -BackgroundColor Black
 Get-PSReadlineKeyHandler -Bound | Select-Object Function, Key | Where-Object {$_.Function -notmatch "DigitArgument"}
