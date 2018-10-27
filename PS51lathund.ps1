824c5970a59afa2ddca0de6b148736ac528d2e06
Stefan <karl_stefan@yahoo.se>
2018-10-27 17:53:24 +0200

    Version 0.1d
    Smärre ändringar.

--- PS51lathund.ps1
+++ PS51lathund.ps1
@@ -1,24 +1,34 @@
 <#
-Lathund för PowerShell 5.1 noviser version 0.1c
+Lathund för PowerShell 5.1 noviser version 0.1d
 © 2018 av Stefan Blecko
 
 Kopiera gärna innehållet i filen till C:\Users\[ditt användarnamn]\Documents\
-WindowsPowerShell\Microsoft.PowerShell_profile.ps1 och testa och se vad som händer.
+WindowsPowerShell\Microsoft.PowerShell_profile.ps1.  
+händer.
 #>
 
 $PSprojfoldername = "PSproj"
 Set-Variable $HOME C:\Users\$env:USERNAME
 
-Write-Host "Användare: $env:USERNAME | Projektmapp: $PSprojfoldername`n" -BackgroundColor DarkBlue
+Write-Host "Användare: $env:USERNAME | Projektmapp: $PSprojfoldername`n"`
+-BackgroundColor DarkBlue
 
 if (Test-Path -Path "C:\Users\$env:USERNAME\Desktop\$PSprojfoldername") {
 }
 else {
-    New-Item -Path "C:\Users\$env:USERNAME\Desktop\" -Name $PSprojfoldername -ItemType "directory" | Out-Null
+    New-Item -Path "C:\Users\$env:USERNAME\Desktop\" -Name $PSprojfoldername `
+    -ItemType "directory" | Out-Null
 }
-New-PSDrive -Name $PSprojfoldername -PSProvider FileSystem -Root "C:\Users\$env:USERNAME\Desktop\$PSprojfoldername" -Description "Välkommen till skript lådan." | Out-Null
+New-PSDrive -Name $PSprojfoldername -PSProvider FileSystem -Root `
+"C:\Users\$env:USERNAME\Desktop\$PSprojfoldername" -Description `
+"Välkommen till skript lådan." | Out-Null
 Set-Location ($PSprojfoldername + ":")
-Write-Host "PÅGÅENDE POWERSHELL PROJEKT" -ForegroundColor White -BackgroundColor Black
-Get-ChildItem *.ps1 | Select-Object Name, LastWriteTime | Sort-Object LastWriteTime -Descending | Out-Host
-Write-Host "KONSOLFÖNSTRETS KORTKOMMANDON" -ForegroundColor White -BackgroundColor Black
-Get-PSReadlineKeyHandler -Bound | Select-Object Function, Key | Where-Object {$_.Function -notmatch "DigitArgument"}
+Write-Host "PÅGÅENDE POWERSHELL PROJEKT" -ForegroundColor White `
+-BackgroundColor Black -NoNewline
+Get-ChildItem *.ps1 | Select @{Name = "Namn"; Expression = {$_.Name}}, 
+@{Name = "Senast ändrad"; Expression = {$_.LastWriteTime}} | sort "Senast ändrad" `
+-Descending | Out-Host
+Write-Host "KONSOLFÖNSTRETS KORTKOMMANDON" -ForegroundColor White `
+-BackgroundColor Black -NoNewline
+Get-PSReadlineKeyHandler -Bound | Select Function, Key | ? {$_.Function -notmatch
+"DigitArgument"} | Format-Table -HideTableHeaders
