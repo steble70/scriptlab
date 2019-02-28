c1b3a06f29667f31dbd1632e425cf54da33c7b15
Stefan <karl_stefan@yahoo.se>
2019-02-28 11:39:21 +0100

    Floskelmaskinen
    Version 0.1b

--- floskelmaskinen.py
+++ floskelmaskinen.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
-
-
-
-
-#Floskelmaskinen version 0.1 (c) 2017 av Stefan
+#Floskelmaskinen version 0.1b (c) 2019 av Stefan Blacko
 
 import random
-
-    
 
 def Floskelmaskinen(How_many_empty_phrases = 1):
     '''
     Man kör programet genom att ange hur många floskler som ska genereras, tex
     Floskelmaskinen(10) = 10 floskler eller Floskelmaskinen(1322) = 1322 floskler.
     '''
-
-
     
     subjekt = ['Sverigedemokrater', 'Moderater', 'Centernpartister', 'Liberaler',
                'Kristdemokrater']
-
 
     predikat = ['vill se tillväxten av', 'vill ha fler', 'skapar tillfällen för',
                 'bekämpar utanförskapet genom','sätter focus på']
@@ -33,7 +24,6 @@ def Floskelmaskinen(How_many_empty_phrases = 1):
               'friheten främst','en valfrihetsrevolutionen', 'ansvar först','arbeten',
               'människor', 'i framtiden', 'landet']                 
 
-
     counter = 0
 
     while counter < How_many_empty_phrases:
@@ -41,7 +31,6 @@ def Floskelmaskinen(How_many_empty_phrases = 1):
         print('Vi', random.choice(subjekt), random.choice(predikat), random.choice(objekt),
               'och', random.choice(objekt) + '.')
 
-    return
 
     
     
