9c9129fc12ab6fe2d4651de5f5a82bb8adec7051
Stefan <karl_stefan@yahoo.se>
2019-05-12 10:10:31 +0200

    Lycka-Olycka med PowerShell

---
+++ Get-DalaiLama.psm1
@@ -0,0 +1,49 @@
+<#
+.SYNOPSIS
+Kalkylerar hur mycket lycka du har i ditt liv. Du får +1 procentenheter för varje sak 
+(handling) som har gjort dig lite mer lycklig eller -1 procentenheter som har gjort 
+dig lite mindre lycklig. 0 procentenheter om ditt lyckotillstånd är oförändrad. Optimal 
+lyckonivå bör ligga något över medel (50%) efter dagens slut. 
+.EXAMPLE
+Get-DalaiLama -defaulthappiness 51 -happines +2 eller help Get-DalaiLama
+.NOTES
+© 2019 av Stefan Blecko
+Kopiera och spara skriptet i mappen C:\Users\[DITT ANVÄNDARNAMN]\Documents\
+PowerShell\Modules\Get-DalaiLama\Get-DalaiLama.psm1. Skripet funkar 
+bara med PowerShell Core och Windows. 
+#>
+function Get-DalaiLama {
+    [cmdletBinding()]
+    param( 
+        [Parameter(Mandatory = $true)]
+        $happiness ,
+        [Parameter(Mandatory = $false)]
+        $defaulthappiness 
+
+    )
+    Write-Output "
+    Om man önskar lycka bör man söka de orsaker som 
+    ger upphov till den och om man inte önskar lidande
+    bör man se till att de orsaker och förhållande som
+    skulle utlösa det inte längre uppträder.
+                                            
+                                            Dalai Lama" 
+
+    # Om $defaulthappiness är satt till större eller lika med 0.
+    if ($defaulthappiness -ge 0) {
+        Set-Content -Value $defaulthappiness -Path "$env:USERPROFILE\Documents\PowerShell\Modules\Get-DalaiLama\H_value"   
+        $read_dh = Get-Content -Path "$env:USERPROFILE\Documents\PowerShell\Modules\Get-DalaiLama\H_value"
+    } 
+    # Annars läs in H_value.
+    elseif (Test-Path -Path "$env:USERPROFILE\Documents\PowerShell\Modules\Get-DalaiLama\H_value") {
+        $read_dh = Get-Content -Path "$env:USERPROFILE\Documents\PowerShell\Modules\Get-DalaiLama\H_value"
+    }
+    # Finns inget $defaulthappiness, sätt $defaulthappiness till 50.
+    else {
+        Set-Content -Value "50" -Path "$env:USERPROFILE\Documents\PowerShell\Modules\Get-DalaiLama\H_value"   
+        $read_dh = Get-Content -Path "$env:USERPROFILE\Documents\PowerShell\Modules\Get-DalaiLama\H_value"
+    }
+    $sum = [int]$read_dh + [int]$happiness
+    Write-Host "`n`nDin nuvarande lyckonivå är $sum%.`n"
+    Set-Content -Value $sum -Path "$env:USERPROFILE\Documents\PowerShell\Modules\Get-DalaiLama\H_value"
+}
