4d40386c96048e7ff654e21687f06d44f9793d78
Stefan <karl_stefan@yahoo.se>
2020-03-23 20:01:36 +0100

    Version 0.2

--- floskelmaskinen.py
+++ floskelmaskinen.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python
-#Floskelmaskinen version 0.1c (c) 2019 av Stefan Blecko
+# floskelmaskinen.py, version 0.2, (c) 2019, 2020 av Stefan Blecko
 
 import random
+import argparse
 
-def Floskelmaskinen(How_many_empty_phrases = 1):
-    '''
-    Man kör programet genom att ange hur många floskler som ska genereras, tex
-    Floskelmaskinen(10) = 10 floskler eller Floskelmaskinen(1322) = 1322 floskler.
-    '''
-    
+def floskel():
     subjekt = ['Sverigedemokrater', 'Moderater', 'Centernpartister', 'Liberaler',
                'Kristdemokrater']
 
     predikat = ['vill se tillväxten av', 'vill ha fler', 'skapar tillfällen för',
                 'bekämpar utanförskapet genom','sätter focus på']
                      
-
     objekt = ['låga ingångslöner', 'arbetslinjen','värdegrunder', 'att göra rätt för sig',
               'flera jobb', 'svenska värderingar', 'ansvar', 'sänkta bidrag', 'trygghet',
-              'fler poliser', 'integration', 'segregationen', 'småföretagen',
+              'fler poliser', 'integration', 'segregationen', 'småföretagande',
               'sänkta ingångslöner', 'bidragsberoende', 'en valfrihet för alla',
               'att ha mera RUT', 'entreprenörer', 'flera enkla jobb',
               'friheten främst','en valfrihetsrevolutionen', 'ansvar först','arbeten',
               'människor', 'i framtiden', 'landet']                 
 
+    return subjekt, predikat, objekt
+
+
+def main():
+    '''
+    Ex:
+    python floskelmaskinen.py (skapar 1 floskel)
+    python floskelmaskinen.py --antal_floskler 43 (skapar 43 floskler)
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--antal_floskler', default=1)
+    args = parser.parse_args()
+
     counter = 0
-
-    while counter < How_many_empty_phrases:
+    while counter < int(args.antal_floskler):
         counter = counter + 1
-        print('Vi', random.choice(subjekt), random.choice(predikat), random.choice(objekt),
-              'och', random.choice(objekt) + '.')
+        print('Vi', random.choice(floskel()[0]), random.choice(floskel()[1]), 
+              random.choice(floskel()[2]), 'och', random.choice(floskel()[2]) + '.')
 
-
-    
-    
+if __name__ == "__main__":
+    main()
